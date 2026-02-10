b9a25f275f484b0ea0c2f18fc0f809849c90c0ce
munshifayeez681-ship-it <munshifayeez681@gmail.com>
2026-02-10 13:31:51 +0530

    Add files via upload

---
+++ frontend/src/data.js
@@ -0,0 +1,23 @@
+export const rankingData = [
+  {
+    rank: 1,
+    name: "Antonina Cassin",
+    experience: 12,
+    skills: "Waste Management, Team Leadership, Sustainability Planning",
+    total: 27
+  },
+  {
+    rank: 2,
+    name: "Saul Gerhold",
+    experience: 13,
+    skills: "Lean Manufacturing, Safety Compliance, Team Leadership",
+    total: 25
+  },
+  {
+    rank: 3,
+    name: "Wendy Rutherford",
+    experience: 10,
+    skills: "Waste Management, Safety Compliance, Team Leadership",
+    total: 23
+  }
+];
